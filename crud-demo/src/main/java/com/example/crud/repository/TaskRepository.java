8f9eb6a1e9d2028da32dbaecb40822080b45ef07
Sameer0712 <sameernimbalkar2001@gmail.com>
2026-07-11 00:15:46 +0530

    Delete crud-demo directory

--- crud-demo/src/main/java/com/example/crud/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
